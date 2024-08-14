19bf3bbf07e87b2a87013f10af9634233bf99ed6
ayaribon <kubotaren5213@docomo.ne.jp>
2024-08-14 22:37:31 +0900

    add:task作成から一覧表示まで

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,10 @@ Rails.application.routes.draw do
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
 
+  resources :travel_plans, only: %i[index new create show edit destroy update] do
+    resources :tasks
+  end
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
