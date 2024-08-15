850065490e8164ef2be7bdbf1ec46e45dc2d789a
ayaribon <kubotaren5213@docomo.ne.jp>
2024-08-15 14:54:40 +0900

    add:taskテンプレート基礎機能追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_14_132057) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_15_054542) do
   create_table "tasks", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title", null: false
     t.text "body"
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_14_132057) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "travel_plan_id"
+    t.boolean "is_template"
     t.index ["travel_plan_id"], name: "index_tasks_on_travel_plan_id"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
